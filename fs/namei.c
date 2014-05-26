136abce627da71573e110e1636aed0dbad4ed83b
Amir Samuelov <amirs@codeaurora.org>
2014-05-26 11:44:06 +0300

    security: selinux: Add Per-File-Encryption hooks
    Add hooks for tagging/detecting Per-File-Encryption files.
    
    Change-Id: I9d1f791b68d3552b1a508c21ff8336182e8527fa
    Signed-off-by: Amir Samuelov <amirs@codeaurora.org>

--- fs/namei.c
+++ fs/namei.c
@@ -2147,6 +2147,13 @@ int vfs_create2(struct vfsmount *mnt, struct inode *dir, struct dentry *dentry,
 	if (error)
 		return error;
 	error = dir->i_op->create(dir, dentry, mode, nd);
+	if (error)
+		return error;
+
+	error = security_inode_post_create(dir, dentry, mode);
+	if (error)
+		return error;
+
 	if (!error)
 		fsnotify_create(dir, dentry);
 	return error;
@@ -2631,6 +2638,13 @@ int vfs_mknod2(struct vfsmount *mnt, struct inode *dir, struct dentry *dentry, u
 		return error;
 
 	error = dir->i_op->mknod(dir, dentry, mode, dev);
+	if (error)
+		return error;
+
+	error = security_inode_post_create(dir, dentry, mode);
+	if (error)
+		return error;
+
 	if (!error)
 		fsnotify_create(dir, dentry);
 	return error;
